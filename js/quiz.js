4f2a9fdba52de1369932186c1265069b40b0dfbf
Temiogundeji <yusuffogundeji@gmail.com>
2020-03-30 22:02:32 +0100

    function added

---
+++ js/quiz.js
@@ -0,0 +1,59 @@
+class Quiz{
+    constructor(){
+        _this = this;
+        _this.isTestEnd = false;
+        _this.isTimeElapse = false;
+        _this.result = [];
+        _this.selectElement();
+        _this.questions = [
+            {
+                id:1,
+                question:"What is machine learning",
+                optionA:"Mathematics of statistics",
+                optionB: "Employing mathematical models to solving life problems",
+                optionC:"Solviing problems by programming mathematical models",
+                optionD: "None of the above",
+                correctOption:"C"
+            },
+            {
+                id:2,
+                question:"What is machine learning",
+                optionA:"Mathematics of statistics",
+                optionB: "Employing mathematical models to solving life problems",
+                optionC:"Solviing problems by programming mathematical models",
+                optionD: "None of the above",
+                correctOption:"C"
+            },
+            {
+                id:3,
+                question:"What is machine learning",
+                optionA:"Mathematics of statistics",
+                optionB: "Employing mathematical models to solving life problems",
+                optionC:"Solviing problems by programming mathematical models",
+                optionD: "None of the above",
+                correctOption:"C"
+            },
+            {
+                id:4,
+                question:"What is machine learning",
+                optionA:"Mathematics of statistics",
+                optionB: "Employing mathematical models to solving life problems",
+                optionC:"Solviing problems by programming mathematical models",
+                optionD: "None of the above",
+                correctOption:"C"
+            }
+        ]
+    }
+    selectElements(){
+        
+    }
+    init(){
+       
+    }
+    loadPrev(){}
+    loadNext(){}
+    checkTimeout(){}
+    calculateResult(){}
+    displayResult(){}
+
+}
